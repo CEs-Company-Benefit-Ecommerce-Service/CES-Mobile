4c16de670a5429fe8ef894e4164a3728cdcd9fdc
Le Thanh Phuoc <thanhphuoc.se@gmail.com>
2023-07-19 14:49:52 +0700

    setup edit profile page

---
+++ lib/app/modules/edit_profile/controllers/edit_profile_controller.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+
+import 'package:ces_app/app/models/account_payload_model.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:http/http.dart' as http;
+
+import '../../wrapper/controllers/wrapper_controller.dart';
+
+class EditProfileController extends GetxController {
+  final editProfileKey = GlobalKey<FormBuilderState>();
+  final WrapperController wrapperController = Get.find<WrapperController>();
+  final box = GetStorage();
+  void editProfile() async {
+    if (editProfileKey.currentState!.saveAndValidate()) {
+      try {
+        String token = box.read("token");
+
+        AccountPayloadModel payload = AccountPayloadModel(
+          name: editProfileKey.currentState?.value['name'],
+          phone: editProfileKey.currentState?.value['phone'],
+          // id: wrapperController.user?.id,
+          email: wrapperController.user.value.email,
+          // address: wrapperController.user?.address,
+          // imageUrl: wrapperController.user?.imageUrl,
+          role: wrapperController.user.value.role,
+          companyId: wrapperController.user.value.companyId,
+        );
+
+        http.Response response = await http.put(
+            Uri.tryParse(
+                'https://api-dev.ces.bio/api/account/${wrapperController.user.value.id}')!,
+            headers: <String, String>{
+              'Content-Type': 'application/json; charset=UTF-8',
+              'Authorization': 'Bearer $token',
+            },
+            body: jsonEncode(payload));
+
+        print(jsonDecode(response.body));
+        await wrapperController.fetchUser();
+        Get.back();
+
+        // if (response.statusCode == 200) {
+        //   var result = jsonDecode(response.body);
+        //   // loginModel = LoginModel.fromJson(result['data']);
+
+        //   Get.offNamed('/wrapper');
+        // } else {
+        //   if (kDebugMode) {
+        //     print(
+        //       'login api + ${response.body}',
+        //     );
+        //   }
+        // }
+      } catch (e) {
+        if (kDebugMode) {
+          print('Error while getting data is $e');
+        }
+      } finally {
+        // isLoading(false);
+      }
+    }
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  // @override
+  // void onReady() {
+  //   super.onReady();
+  // }
+
+  // @override
+  // void onClose() {
+  //   super.onClose();
+  // }
+}
---
+++ lib/app/modules/edit_profile/views/edit_profile_view.dart
@@ -0,0 +1,116 @@
+import 'package:ces_app/app/modules/wrapper/controllers/wrapper_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/edit_profile_controller.dart';
+
+class EditProfileView extends GetView<EditProfileController> {
+  const EditProfileView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final WrapperController wrapperController = Get.find<WrapperController>();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.red,
+          elevation: 0.5,
+          title: const Text(
+            'Edit Info',
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  controller.editProfile();
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                  backgroundColor: Colors.red,
+                ),
+                child: const Text("Save"),
+              ),
+            )
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: FormBuilder(
+            key: controller.editProfileKey,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 16),
+                  const Row(
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "Avatar",
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          SizedBox(height: 8),
+                          CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.red,
+                            foregroundColor: Colors.white,
+                            child: Icon(Icons.person),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            "Add photo",
+                            style: TextStyle(fontSize: 10),
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: 16),
+                      Flexible(
+                          child: Text(
+                              "Choose the best photo! Because everyone will be able to see this photo"))
+                    ],
+                  ),
+                  const SizedBox(height: 32),
+                  const Text("Name"),
+                  const SizedBox(height: 4),
+                  FormBuilderTextField(
+                    name: 'name',
+                    initialValue: wrapperController.user.value.name,
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.required(),
+                    ]),
+                    decoration: const InputDecoration(
+                      fillColor: Colors.transparent,
+                      filled: true,
+                      hintText: "Name",
+                      contentPadding: EdgeInsets.all(0),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const Text("Phone"),
+                  const SizedBox(height: 4),
+                  FormBuilderTextField(
+                    name: 'phone',
+                    initialValue: wrapperController.user.value.phone,
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.required(),
+                    ]),
+                    decoration: const InputDecoration(
+                      fillColor: Colors.transparent,
+                      filled: true,
+                      hintText: "Name",
+                      contentPadding: EdgeInsets.all(0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
